4f87d045247c40f559265616cf3968a12e2d5b02
Shrinidhi13 <120328689+Shrinidhi13@users.noreply.github.com>
2023-11-04 10:46:00 +0530

    added changes

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function searchProperty() {
+    var propertyType = document.getElementById("propertyType").value;
+    var location = document.getElementById("location").value;
+
+    // Perform the search based on propertyType and location
+    // You can implement your search logic here
+    // For this example, we'll just display an alert with the selected values
+    alert("Searching for " + propertyType + " in " + location);
+}
+
